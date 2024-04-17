28b4d300e3acfce4037e2002a06032b2489c7e7b
t6adev <t.yamamoto28@gmail.com>
2024-04-18 08:11:09 +0900

    manage issues

--- src/pages/demo2.tsx
+++ src/pages/demo2.tsx
@@ -2,16 +2,16 @@ import { Link } from '../components/viewTransition/Link.js';
 
 export default function Page() {
   return (
-    <div className="demo-box">
+    <div className="demo-box2">
       <h2>
-        This is the <span className="demo">Demo 1</span>
+        This is the <span className="demo">Demo2</span>
       </h2>
       <p>OK you just saw the demo :)</p>
       <Link to="/" className="block">
         Open homepage →
       </Link>
-      <Link to="/demo2" className="block">
-        Open Demo 2 →
+      <Link to="/demo" className="block">
+        Open Demo 1 →
       </Link>
     </div>
   );
