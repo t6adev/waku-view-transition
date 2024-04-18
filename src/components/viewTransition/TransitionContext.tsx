4c1042e7c311af6388cae8c7aebcd952524f452a
t6adev <t.yamamoto28@gmail.com>
2024-04-18 12:08:33 +0900

    chore

--- src/components/viewTransition/TransitionContext.tsx
+++ src/components/viewTransition/TransitionContext.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+/**
+ * The original is https://github.com/shuding/next-view-transitions/blob/b1e1211e95f36eaca60f7540b54d220773f8921d/src/transition-context.tsx
+ */
+
 import type { Dispatch, SetStateAction } from 'react';
 import { createContext, use, useEffect, useState } from 'react';
 import { useBrowserNativeTransitions } from './browser-native-events.js';
@@ -32,44 +36,3 @@ export function ViewTransitions({
 export function useSetFinishViewTransition() {
   return use(ViewTransitionsContext);
 }
-
-/**
- * The original is https://github.com/shuding/next-view-transitions/blob/b1e1211e95f36eaca60f7540b54d220773f8921d/src/transition-context.tsx
- * But it's not working
- */
-
-// import type { Dispatch, SetStateAction } from 'react';
-// import { createContext, use, useEffect, useState } from 'react';
-
-// import { useBrowserNativeTransitions } from './browser-native-events.js';
-
-// const ViewTransitionsContext = createContext<Dispatch<SetStateAction<(() => void) | null>>>(
-//   () => () => {}
-// );
-
-// export function ViewTransitions({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   const [finishViewTransition, setFinishViewTransition] = useState<null | (() => void)>(null);
-
-//   useEffect(() => {
-//     if (finishViewTransition) {
-//       finishViewTransition();
-//       setFinishViewTransition(null);
-//     }
-//   }, [finishViewTransition]);
-
-//   useBrowserNativeTransitions();
-
-//   return (
-//     <ViewTransitionsContext.Provider value={setFinishViewTransition}>
-//       {children}
-//     </ViewTransitionsContext.Provider>
-//   );
-// }
-
-// export function useSetFinishViewTransition() {
-//   return use(ViewTransitionsContext);
-// }
